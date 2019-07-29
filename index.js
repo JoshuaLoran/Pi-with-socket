b06ce5d995c090fea78dbb37e1f443585a4fd5a9
Joshua Loran <joshua.loran@gmail.com>
2019-07-29 14:36:15 -0700

    added user ID variables

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED1 = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
-// var LED17 = new Gpio(17, 'out');
-var pushButton = new Gpio(17, 'in', 'both');
 const WebSocket = require('ws')
 const url = 'http://agile-reef-99245.herokuapp.com/cable'
 const connection = new WebSocket(url)
 
+const onOffDeviceId = null  // Replace "null" with the device ID from Home Control, connect to pin 4 on your RaspberryPi
+const alarmDeviceId = null // Replace "null" with the device ID from Home Control, connect to pin 17 on your RaspberryPi
+const multiDeviceId = null  // Replace "null" with the device ID from Home Control, connect to pin 27 on your RaspberryPi
 
-
+var PIN1 = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+var PIN2 = new Gpio(27, 'out'); //use GPIO pin 27, and specify that it is output
+var pushButton = new Gpio(17, 'in', 'both'); //define GPIO pin 17 as an input
 
     const msg = {
-        command: 'subscribe',
-        identifier: JSON.stringify({
-            channel: 'DevicesChannel'
-        })
+    command: 'subscribe',
+    identifier: JSON.stringify({
+        channel: 'DevicesChannel'
+    })
     }
     connection.onopen = () => {
         connection.send(JSON.stringify(msg))
@@ -29,46 +31,47 @@ const connection = new WebSocket(url)
         if (typeof data.message === 'object'){
             controlDevices(data.message)
             console.log(data.message)
-        } 
-    
+        }
+
     }
 
-    pushButton.watch(function (err, value){
+    alarmTrigger.watch(function (err, value){
         if(err){
             console.log('There was an error', err)
             return
         }
-        if(value === 1){
+        if(value === alarmDeviceId){
             sendDeviceNews('off')
         }
-        
+
     })
 
     function unexportOnClose(){
-        LED1.writeSync(0);
-        LED1.unexport();
-        pushButton.unexport();
+        PIN1.writeSync(0);
+        PIN1.unexport();
+        PIN2.writeSync(0);
+        PIN2.unexport();
+        alarmTrigger.unexport();
     }
 
     process.on('SIGINT', unexportOnClose);
 
     function controlDevices(device){
-        console.log('Device: ',device)
-        if(device.id === 1){
+        if(device.id === onOffDeviceId){
             if(device.commands[0]==='on'){
-                LED1.writeSync(1)
+                PIN1.writeSync(1)
             } else {
-                LED1.writeSync(0)
+                PIN1.writeSync(0)
             }
-            } 
-        //     if(device.id === 2){
-        //     if(device.commands[0]==='on'){
-        //         LED17.writeSync(1)
-        //     } else {
-        //         LED17.writeSync(0)
-        //     }
-        // }
-    
+            }
+            if(device.id === multiDeviceId){
+            if(device.commands[0]==='on'){
+                PIN2.writeSync(1)
+            } else {
+                PIN2.writeSync(0)
+            }
+        }
+
     }
 
     function sendDeviceNews(state){
@@ -78,19 +81,10 @@ const connection = new WebSocket(url)
                 channel: 'DevicesChannel'
             }),
             data: JSON.stringify({
-                id: 10,
+                id: alarmDeviceId,
                 commands: [state]
             })
         }
         console.log(sendData)
         connection.send(JSON.stringify(sendData))
     }
-      
-
-
-
-
-
-
-
-
